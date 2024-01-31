5f1fb1a8d4c877da5a5c5b138546b242f32bfdb6
Joshal007 <joshaljoshi07@gmail.com>
2024-02-01 01:06:35 +0530

    Building Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from './components/header/Header';
+import About from './components/page/About';
+import Tasks from './components/task/ViewTask';
+import AddTask from './components/task/AddTask';
+import UpdateTask from './components/task/UpdateTask';
+import Login from './components/auth/Login';
+import Register1 from './components/auth/Register1';
+import Logout from './components/auth/Logout';
+import Landing from './components/page/Landing';
+import NotFound from './components/page/NotFound';
+
+import './bootstrap.min.css';
 import './App.css';
 
-function App() {
+function App () {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if(sessionStorage.getItem('token') !== null){
+      setIsAuthenticated(true);
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
+        <Routes>
+          <Route path="/" element={<Landing isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/signin" element={<Login isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/signup" element={<Register1 isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/signout" element={<Logout isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/todo" element={<Tasks isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/add" element={<AddTask isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/update/:id" element={<UpdateTask isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
